5ba07594d7e632a7961355a42659a2423627a159
Avinash1705 <avinashrawat1705@gmail.com>
2019-11-15 13:52:49 +0530

    icon change

--- app/src/main/java/com/example/avinash/calculator/MainActivity.java
+++ app/src/main/java/com/example/avinash/calculator/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         add=findViewById(R.id.sum);
         mul=findViewById(R.id.mul);
         div=findViewById(R.id.div);
